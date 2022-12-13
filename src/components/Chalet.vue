52de88ed1a226fd11a23cceb8998c892f92c6193
clement.t <clement.tolois@gmail.com>
2022-12-13 12:23:52 +0100

    Chore: prepare chalet component

---
+++ src/components/Chalet.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="chalet-wrapper">
+        <x3d width="100%" height="800px">
+            <scene>
+                <transform translation="0 0 0">
+                    <shape>
+                        <appearance> 
+                            <ImageTexture url="/textures/wood.png"/>
+                        </appearance> 
+                        <box size="1 1 1"></box>
+                    </shape>
+                </transform>
+            </scene>
+        </x3d>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,9 @@
 <template>
     <div class="home">
-        <x3d width="100%" height="800px">
-            <scene>
-                <transform translation="0 0 0">
-                    <shape>
-                        <appearance>
-                            <material diffuseColor="1 0 0"></material>
-                        </appearance>
-                        <box size="1 1 1"></box>
-                    </shape>
-                </transform>
-            </scene>
-        </x3d>
+        <Chalet/>
     </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Chalet from "@/components/Chalet.vue";
+</script>
