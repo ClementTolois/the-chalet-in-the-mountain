fa6053fc53bb5faa99078406595e40b567226ed8
clement.t <clement.tolois@gmail.com>
2022-12-13 18:49:29 +0100

    Chore: try to load an homemade blender model

--- src/components/Chalet.vue
+++ src/components/Chalet.vue
@@ -3,12 +3,13 @@
         <x3d width="100%" height="800px">
             <scene>
                 <transform translation="0 0 0">
-                    <shape>
+                    <!-- <shape>
                         <appearance> 
                             <ImageTexture url="/textures/wood.png"/>
                         </appearance> 
                         <box size="1 1 1"></box>
-                    </shape>
+                    </shape> -->
+                    <inline nameSpaceName="Chalet" mapDEFToID="true" url="/models/chalet.x3d" >  </inline>
                 </transform>
             </scene>
         </x3d>
