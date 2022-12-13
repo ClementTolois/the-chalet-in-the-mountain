e50888d39249f9f05531851b16eccc32bdf15c8b
clement.t <clement.tolois@gmail.com>
2022-12-13 08:29:00 +0100

    Chore: Init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <x3d width="100%" height="800px">
+            <scene>
+                <transform translation="0 0 0">
+                    <shape>
+                        <appearance>
+                            <material diffuseColor="1 0 0"></material>
+                        </appearance>
+                        <box size="1 1 1"></box>
+                    </shape>
+                </transform>
+            </scene>
+        </x3d>
+    </div>
+</template>
+
+<script lang="ts" setup></script>
